a39f222521ead56ab5cf0cd5447c0bfba185a87f
Zane Shannon <zcs@smileslaughs.com>
2016-09-26 22:39:58 -0700

    refactored exec to build security group and public ip, run, then tear down :)

--- lib/shiprails/ship/exec.rb
+++ lib/shiprails/ship/exec.rb
@@ -64,7 +64,6 @@ module Shiprails
         task_definition_arn = task_descriptions.tasks.first.task_definition_arn
         task_definition_name = task_definition_arn.split('/').last
         container_instance_arn = task_descriptions.tasks.first.container_instance_arn
-
         task_definition_description = ecs.describe_task_definition({task_definition: task_definition_name})
 
         say "Setting up EC2 instance for SSH..."
@@ -72,31 +71,39 @@ module Shiprails
         ec2_instance_id = ecs.describe_container_instances({cluster: cluster, container_instances: [container_instance_arn]}).container_instances.first.ec2_instance_id
         ec2_instance = ec2.describe_instances({instance_ids: [ec2_instance_id]}).reservations.first.instances.first
 
-        puts ec2_instance.inspect
+        # get its current security groups to restory later
+        security_group_ids = ec2_instance.security_groups.map(&:group_id)
 
-        exit
-
+        # create new public ip
         elastic_ip = ec2.allocate_address({ domain: "vpc" })
+        # link ip to ec2 instance
         associate_address_response = ec2.associate_address({
           allocation_id: elastic_ip.allocation_id,
           instance_id: ec2_instance_id
         })
+        # create security group for us
         security_group_response = ec2.create_security_group({
           group_name: "shiprails-exec-#{cluster}-#{Time.now.to_i}",
-          description: "SSH access to run interactive command (created by `whoami`; shiprails)",
+          description: "SSH access to run interactive command (created by #{`whoami`.rstrip} via shiprails)",
           vpc_id: ec2_instance.vpc_id
         })
+        # get our public ip
         my_ip_address = open('http://whatismyip.akamai.com').read
+        # authorize SSH access from our public ip
         ec2.authorize_security_group_ingress({
           group_id: security_group_response.group_id,
           ip_protocol: "tcp",
-          from_port: -1,
-          to_port: "22",
+          from_port: 22,
+          to_port: 22,
           cidr_ip: "#{my_ip_address}/32"
         })
+        # add ec2 instance to our new security group
+        ec2.modify_instance_attribute({
+          instance_id: ec2_instance_id,
+          groups: security_group_ids + [security_group_response.group_id]
+        })
 
-        # TODO: add security group to instance ec2_instance_id
-
+        # build the command we'll run on the instance
         command_array = ["docker run -it --rm"]
         task_definition_description.task_definition.container_definitions.first.environment.each do |env|
           command_array << "-e #{env.name}='#{env.value}'"
@@ -105,15 +112,26 @@ module Shiprails
         command_array << command
         command_string = command_array.join ' '
 
-        say "Connecting to #{ec2_instance_id}..."
+        say "Waiting for AWS to setup networking..."
+        sleep 5 # AWS just needs a little bit to setup networking
+        say "Connecting #{ssh_user}@#{elastic_ip.public_ip}..."
         say "Executing: $ #{command_string}"
-        exec "ssh -o ConnectTimeout=5 -t -i #{ssh_private_key_path} #{ssh_user}@#{elastic_ip.public_ip} '#{command_string}'"
-
+        system "ssh -o ConnectTimeout=15 -o 'StrictHostKeyChecking no' -t -i #{ssh_private_key_path} #{ssh_user}@#{elastic_ip.public_ip} '#{command_string}'"
+      rescue => e
+        say "Error: #{e.message}", :red
+      ensure
         say "Cleaning up SSH access..."
-        # TODO: remove security group from instance ec2_instance_id
-        ec2.delete_security_group({ group_id: security_group_response.group_id })
-        ec2.disassociate_address({ association_id: associate_address_response.association_id })
-        ec2.release_address({ allocation_id: elastic_ip.allocation_id })
+        # restore original security groups
+        ec2.modify_instance_attribute({
+          instance_id: ec2_instance_id,
+          groups: security_group_ids
+        }) rescue nil
+        # remove our access security group
+        ec2.delete_security_group({ group_id: security_group_response.group_id }) rescue nil
+        # unlink ec2 instance from public ip
+        ec2.disassociate_address({ association_id: associate_address_response.association_id }) rescue nil
+        # release public ip address
+        ec2.release_address({ allocation_id: elastic_ip.allocation_id }) rescue nil
         say "Done.", :green
       end
 
